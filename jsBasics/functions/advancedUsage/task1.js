190fcdda7daa1f6003fa8f9b8a221d0580feaf9a
Oleksii Kudenko <okudenko@rein.ai>
2024-11-16 19:35:49 +0200

    added hw7

---
+++ jsBasics/functions/advancedUsage/task1.js
@@ -0,0 +1,14 @@
+function handleNum(number, handleNum, handleOdd) {
+  return number % 2 === 0 ? handleEven() : handleOdd();
+}
+
+function handleEven() {
+  return "number is even";
+}
+
+function handleOdd() {
+  return "number is odd";
+}
+
+console.log(handleNum(2, handleEven, handleOdd));
+console.log(handleNum(5, handleEven, handleOdd));
