41687df3e0524f3c2aff22d150e1760b8c4aa34b
Oleksii Kudenko <okudenko@rein.ai>
2024-11-22 15:09:43 +0200

    added hw8

---
+++ jsBasics/arrays/task1.js
@@ -0,0 +1,19 @@
+const numbers = [2, -5, 0, 7, -3, 0, 10, -8];
+
+let positiveCount = 0,
+  negativeCount = 0,
+  zeroCount = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] < 0) {
+    negativeCount++;
+  } else if (numbers[i] === 0) {
+    zeroCount++;
+  } else {
+    positiveCount++;
+  }
+}
+
+console.log(negativeCount);
+console.log(positiveCount);
+console.log(zeroCount);
---
+++ jsBasics/arrays/task2.js
@@ -0,0 +1,7 @@
+const firstArray = [1, 2, 3, 4, 5];
+const mutatedArray = [];
+
+for (let i = 0; i < firstArray.length; i++) {
+    mutatedArray.push(firstArray[i] * i);
+}
+console.log(mutatedArray);
